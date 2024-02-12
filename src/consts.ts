e6ae8e9a41db48aa68e9b0eb0685e8d4d818c2de
Jonathan Cheng <39739500+jjjjjjonathan@users.noreply.github.com>
2024-02-12 13:35:30 -0500

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'L1ON Test';
+export const SITE_DESCRIPTION = 'Pretending this is League1 Ontario';
